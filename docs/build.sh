35778a8a401f716231b5643b267c03cc87b59b0b
matt <matt.northstar@gmail.com>
2019-12-19 13:58:58 -0800

    updated hyphens in blog content and templates to underscores and rebuilt

--- docs/build.sh
+++ docs/build.sh
@@ -2,8 +2,8 @@
 
 cat ./templates/side_index.html ./templates/top_index.html ./content/content_index.html > ./index.html
 
-cat ./templates/side_blog-post_Cubell.html ./content/blog-post_Cubell_content.html > ./built_blog_post.html
+cat ./templates/side_blog_post_Cubell.html ./content/blog_post_Cubell_content.html > ./built_blog_post.html
 
 cat ./templates/side_about.html ./content/content_about.html>./built_about.html
 
-cat ./templates/side_blog-list_Cubell.html ./content/blog-list_Cubell_content.html>./built_blog_list.html
+cat ./templates/side_blog_list_Cubell.html ./content/blog_list_Cubell_content.html>./built_blog_list.html
